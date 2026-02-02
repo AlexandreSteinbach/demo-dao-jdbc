40ce5eed7b20edeec0bef2fc0a5abfa798c1c7d5
AlexandreSteinbach <alexandresteinbach2@gmail.com>
2026-02-02 17:58:33 -0300

    findAll department

--- src/model/application/Program2.java
+++ src/model/application/Program2.java
@@ -3,7 +3,10 @@ package model.application;
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
+import model.entities.Seller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Program2 {
@@ -13,10 +16,19 @@ public class Program2 {
 
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
+        List<Department> list = new ArrayList<>();
+
         System.out.println("=== TEST 1: department findById ===");
         Department dep = departmentDao.findById(2);
 
         System.out.println(dep);
 
+        System.out.println("\n=== TEST 2: department findAll ===");
+        list = departmentDao.findAll();
+        for (Department obj : list) {
+            System.out.println(obj);
+        }
+
+        sc.close();
     }
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -7,7 +7,10 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
 
@@ -58,8 +61,40 @@ public class DepartmentDaoJDBC implements DepartmentDao {
         }
     }
 
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("Id"));
+        dep.setName(rs.getString("Name"));
+        return dep;
+    }
+
     @Override
     public List<Department> findAll() {
-        return List.of();
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                    "SELECT * FROM department "
+                    + "ORDER BY Id");
+
+            rs = st.executeQuery();
+
+            List<Department> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while (rs.next()) {
+                Department dep = map.get(rs.getInt("Id"));
+                Department obj = instantiateDepartment(rs);
+                list.add(obj);
+            }
+            return list;
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+
     }
 }
